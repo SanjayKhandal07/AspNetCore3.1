200978ec549f0bd1b3b559edfeebe8811013c663
HP <DESKTOP-EFTRQUL>
2024-12-02 20:36:26 +0400

    Add MVC in Project

--- BookStore/BookStore/Program.cs
+++ BookStore/BookStore/Program.cs
@@ -1,6 +1,19 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+//var builder = WebApplication.CreateBuilder(args);
+//var app = builder.Build().Run();
 
-app.MapGet("/", () => "Hello World!");
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
-app.Run();
+//app.Run();
+
+using BookStore;
+class Program {
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+     Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(WebBuilder => {
+        WebBuilder.UseStartup<Startup>();
+    });
+    }
---
+++ BookStore/BookStore/Startup.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+
+namespace BookStore
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection service) { 
+        service.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("This is the first pieline");
+            //    await next();
+            //    await context.Response.WriteAsync("This is the first 2 pieline");
+            //});
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("This is the Second pipeline");
+            //    await next();
+            //    await context.Response.WriteAsync("This is the Second 2 pipeline");
+            //    await next();
+            //});
+
+            //app.UseRouting();
+            //app.UseEndpoints(endpointRoute =>
+            //{
+            //    endpointRoute.Map("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync(env.EnvironmentName);
+            //    });
+            //});
+
+            //app.UseEndpoints(endpointRoute =>
+            //{
+            //    endpointRoute.Map("/Sanjay", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello ------ Sanjay");
+            //    });
+            //});
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints => {
+            endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
